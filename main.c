9c1da0bf8032112d212233f6c82ebd7c55485368
Xudong Huang <xudongh@juniper.net>
2014-07-01 17:50:55 +0800

    add mbuf test

--- main.c
+++ main.c
@@ -5,11 +5,26 @@
 #include "uio.h"
 #include "fpga_drv.h"
 #include "hp_malloc.h"
-#include "rte/rte_ring.h"
+#include "mbuf.h"
+
+void test_mbuf()
+{
+    mbuf_pool_t* pool = mbuf_pool_alloc(1024);
+
+    DBG_PRINT(*pool);
+
+    void* mbuf = mbuf_get(pool);
+
+    mbuf_put(pool, mbuf);
+
+    mbuf_pool_free(pool);
+}
 
 void test_hp_malloc()
 {
-    hp_t* p_hp = malloc_huge_pages(128 * 1024);
+    hp_t* p_hp = hp_alloc(128 * 1024);
+
+    DBG_PRINT(sizeof(struct rte_ring));
 
     if (p_hp == NULL)
     {
@@ -19,7 +34,7 @@ void test_hp_malloc()
 
     DBG_PRINT(*p_hp);
 
-    free_huge_pages(p_hp);
+    hp_free(p_hp);
 }
 
 void test_fpga_uio()
@@ -40,6 +55,7 @@ void test_fpga_uio()
 int main()
 {
     test_hp_malloc();
+    test_mbuf();
     test_fpga_uio();
     return 0;
 }
